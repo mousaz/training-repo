4d4b9afe472cd6bf38213d96f8c4b82df2029caa
unknown <mismail@sd-pc-143.asaltech.com>
2012-09-09 11:53:22 +0300

    ?

--- mohammad-ismail/routes/jobs.js
+++ mohammad-ismail/routes/jobs.js
@@ -33,7 +33,6 @@ exports.list = function(req1, res1){
                  var link=result["channel"].item[i].link;
                  jsonObj.push({"title": title,"link":link, "Description": Description,"publishDate":PubDate});
                  }
-                //var jsonR=haveJson(itemsArray,jsonObj);
                 var jsonR={"jobs":jsonObj};
 
                 console.log("it work");
@@ -45,23 +44,4 @@ exports.list = function(req1, res1){
     });
 };
 
-
-/*
-function haveJson(itemsArray1,jasonResult){
-    console.log(itemsArray1);
-    var item=itemsArray1;
-    if(item.length==0)return jasonResult;
-    else{
-        console.log(item);
-        var title=item[item.length-1].title;
-        var Description=item[item.length-1].description;
-        var PubDate=item[item.length-1].pubDate;
-        var link=item[item.length-1].link;
-        jasonResult.push({"title": title,"link":link, "Description": Description,"publishDate":PubDate});
-        item.pop();
-        return haveJson(item,jasonResult);
-
-    }
-}
-*/
 
