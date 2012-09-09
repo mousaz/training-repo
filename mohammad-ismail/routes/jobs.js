96044106453d7a98b18532f26e9c8c1c02d7dc1a
mohammad ismail <mismail@asaltech.com>
2012-09-09 15:03:27 +0300

    replace the xml2js lib v1.4 to xml2js 2.0 required by app

--- mohammad-ismail/routes/jobs.js
+++ mohammad-ismail/routes/jobs.js
@@ -12,8 +12,10 @@ exports.list = function(req1, res1){
 
     //Tell the request that we want to fetch youtube.com, send the results to a callback function
 
+//'http://jobs.ps/rss.xml'
+    var req = http.get('http://jobs.ps/rss.xml',function(res) {
 
-    var req = http.get('http://jobs.ps/rss.xml', function(res) {
+
         var pageData = "";
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
@@ -33,6 +35,7 @@ exports.list = function(req1, res1){
                  var link=result["channel"].item[i].link;
                  jsonObj.push({"title": title,"link":link, "Description": Description,"publishDate":PubDate});
                  }
+                //var jsonR=haveJson(itemsArray,jsonObj);
                 var jsonR={"jobs":jsonObj};
 
                 console.log("it work");
@@ -44,4 +47,23 @@ exports.list = function(req1, res1){
     });
 };
 
+
+/*
+function haveJson(itemsArray1,jasonResult){
+    console.log(itemsArray1);
+    var item=itemsArray1;
+    if(item.length==0)return jasonResult;
+    else{
+        console.log(item);
+        var title=item[item.length-1].title;
+        var Description=item[item.length-1].description;
+        var PubDate=item[item.length-1].pubDate;
+        var link=item[item.length-1].link;
+        jasonResult.push({"title": title,"link":link, "Description": Description,"publishDate":PubDate});
+        item.pop();
+        return haveJson(item,jasonResult);
+
+    }
+}
+*/
 
