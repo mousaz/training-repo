f023de3c5a7709c4bfaf2cb8fce5e12f491ae0bc
mohammad ismail <mismail@asaltech.com>
2012-09-11 09:34:15 +0300

    all modifed cja

--- mohammad-ismail/app.js
+++ mohammad-ismail/app.js
@@ -1,29 +1,22 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express'), http = require('http'), path = require('path'), jobs = require('./routes/jobs.js');
+var express = require('express')
+    , jobs = require('./routes/jobs')
+    , http = require('http')
+    , path = require('path');
 
 var app = express();
-
-app.configure(function () {
-  app.set('port', 8080);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+    app.set('port', process.env.PORT || 8080);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
-
 app.configure('development', function(){
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 });
-
 app.get('/jobs', jobs.list);
+
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+    console.log("Express server listening on port " + app.get('port'));
 });
--- mohammad-ismail/routes/jobs.js
+++ mohammad-ismail/routes/jobs.js
@@ -1,35 +1,34 @@
-var http = require('http');
-var xml2js = require('xml2js');
-exports.list = function (req1, res1) {
-    var parser = new xml2js.Parser ();
-    parser.on('end', function (result) {
-        eyes.inspect(result);
-    });
-    var req = http.get('http://jobs.ps/rss.xml', function (res) {
-        var pageData = "";
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            pageData += chunk;
-        });
-        res.on('end', function () {
-            var data = new Buffer(pageData).toString();
-            var parser = new xml2js.Parser();
-            parser.addListener('end', function (result) {
-                var jsonObj = [];
-                var itemsArray = result['channel'].item;
-                itemsArray.forEach(function (item) {
-                    var title = item.title;
-                    var Description = item.description;
-                    var PubDate = item.pubDate;
-                    var link = item.link;
-                    jsonObj.push({"title":title, "link":link, "description":Description, "publishDate":PubDate});
-                });
-                var jsonR = {"jobs":jsonObj};
-                console.log("it work");
-                res1.send(jsonR);
+var fs = require('fs')
+    ,http=require('http')
+    ,xml2js=require('xml2js')
+exports.list = function(request, response){
+    var parser = new xml2js.Parser();
+    try{
+        var req = http.get('http://jobs.ps/rss.xml', function(res) {
+            var pageData = "";
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                pageData += chunk;
             });
-            parser.parseString(data);
-        });
-    });
+            res.on('end', function(){
+                var data=new Buffer(pageData).toString();
+                parser.addListener('end', function(result) {
+                    var jsonObj = [];
+                    var itemsArray=result.channel.item;
+                    itemsArray.forEach(function(item){
+                        var jobs={
+                            title: item.title
+                            , Description: item.description
+                            , PubDate: item.pubDate
+                            , link : item.link};
+                        jsonObj.push(jobs);
+                    });
+                    var finalResult={"jobs":jsonObj};
+                    console.log("it work");
+                    response.send(JSON.stringify(finalResult));
+                });
+                parser.parseString(data);
+            });
+        });}catch (ex){response.end("there is an error");}
 };
 
