d743694319ba740237817f639095dbe99870d0a8
Alswimah <aswimah@asaltech.com>
2012-09-12 14:10:38 +0300

    modifications on news feed according to Dima's comments

--- amal-alswimah/taskv1.js
+++ amal-alswimah/taskv1.js
@@ -1,52 +1,49 @@
-var express=require("express");
-jobApp=module.exports = express(),
-request=require("request"),
-xml2js = require('xml2js'),
-parser = new xml2js.Parser();
-
-jobApp.get("/news", function( req, res)
-{
-    try{
-        var url="http://feeds.bbci.co.uk/news/rss.xml";
-        request(url, function( error, internalresponse, body)
+var express = require("express"),
+    newsApp = module.exports = express(),
+    request = require("request"),
+    xml2js = require('xml2js'),
+    parser = new xml2js.Parser();
+newsApp.get("/news", function (req, res) {
+    try {
+        var url = "http://feeds.bbci.co.uk/news/rss.xml";
+        request(url, function (error, internalresponse, body)
         {
-            if( error)
+            if (error)
             {
-                res.end("there is an error , please try again .");
+                res.end(" ERROR : The connection with  ' www.feeds.bbci.co.uk ' can not be established .");
+            }
+            else if(internalresponse.statusCode !== 200)
+            {
+                res.end(" ERROR : The connection with ' www.feeds.bbci.co.uk ' can not be established .\n\n  StatusCode = " + internalresponse.statusCode);
             }
             else
-            if(internalresponse.statusCode!=200)
             {
-                res.end("there is an error , please try again  ." + internalresponse.statusCode);
-            }
-            else
-            {
-                parser.parseString( body, function( err, result)
+                parser.parseString(body, function(err, result)
                 {
-                    var newsItemsLength = result.rss.channel[0].item.length;
-                    var newsItem= [];
-                    for(i=0;i<newsItemsLength;i++)
+                    var newsItem = [];
+                    (result.rss.channel[0].item).forEach(function (oneItem)
                     {
-                        newsItem[i]=
+                        oneItem =
                         {
-                            title:result.rss.channel[0].item[i].title[0],
-                            link:result.rss.channel[0].item[i].link[0],
-                            description:result.rss.channel[0].item[i].description[0]
+                            title:oneItem.title[0],
+                            link:oneItem.link[0],
+                            description:oneItem.description[0]
                         };
-                    }
-                    var jO=
+                        newsItem.push(oneItem)
+                    } )
+
+
+                    var allNewsItem =
                     {
                         newsArray:newsItem
                     };
-                    res.writeHead(200, {"Content-Type": "application/json; charset=UTF-8"});
-                    res.write(" "+JSON.stringify(jO));
-                    res.end();
+                    res.json (allNewsItem);
                 })
             }
         })}
     catch(exception)
     {
-        res.end("there is an error , please try again .");
+        res.end(" An exception occurred while establishing connection .\n\n Exception type is : "+exception+" .");
     }
-})
-jobApp.listen('8080');
+ } )
+newsApp.listen('8080');
