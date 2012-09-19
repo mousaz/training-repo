00c0b67cac7f6d242a8501f5020c478ea16a8e7b
Alswimah <aswimah@asaltech.com>
2012-09-19 11:21:07 +0300

    modifications on testWeatherFeed according to Dima's comments .

--- amal-alswimah/testWeather.js
+++ amal-alswimah/testWeather.js
@@ -10,28 +10,27 @@ module.exports =
     {
         callback();
     },
-    defaultWeatherFeedTest: function (test)
+    testDefaultWeather: function (test)
     {
         request("http://127.0.0.1:8890/weather?location=ramallah", function(error, response, body)
             {
                 test.equal(error, null, "Error occurred as not expected : " + error + " .");
-                test.equal(response.statusCode, 200, "statusCode occurred as not expected : " + response.statusCode + " .");
+                test.equal(response.statusCode, 200, "Status code is not as expected : " + response.statusCode + " .");
                 var weatherFeed = JSON.parse(body);
                 test.ok(weatherFeed, "weatherFeed object should not refer to null");
-                test.equal(Array.isArray(weatherFeed), false, "the structure of weatherFeed variable should be an object .");
-                test.ok(weatherFeed.temperature, "the temperature property should not be undefined .");
-                test.ok(weatherFeed.skytext, "the skytext property should not be undefined .");
+                test.ok(weatherFeed.temperature, "the temperature property should be definedd .");
+                test.ok(weatherFeed.skytext, "the skytext property should be defined .");
                 test.done();
             }
         );
 
     },
-    invalidWeatherFeedTest: function (test)
+    testInvalidWeather: function (test)
     {
         request("http://127.0.0.1:8890/weather?location=cccccccccccccaaaaaaaaaaaaaaaaabbbbbbbbbb", function(error, response, body)
             {
                 test.equal(error, null, "Error occurred as not expected : " + error + " .");
-                test.equal(response.statusCode, 400, "statusCode occurred as not expected : " + response.statusCode + " .");
+                test.equal(response.statusCode, 400, "Status code is not as expected : " + response.statusCode + " .");
                 var weatherFeed = JSON.parse(body);
                 if (weatherFeed) {
                     test.ok(weatherFeed.statusCode, "StatusCode property should be defined .");
@@ -41,12 +40,12 @@ module.exports =
             }
         );
     },
-    incompleteWeatherFeedTest: function (test)
+    testIncompleteWeather: function (test)
     {
         request("http://127.0.0.1:8890/weather?location=", function(error, response, body)
             {
                 test.equal(error, null, "Error occurred as not expected : " + error + " .");
-                test.equal(response.statusCode, 400, "statusCode occurred as not expected : " + response.statusCode + " .");
+                test.equal(response.statusCode, 400, "Status code is not as expected : " + response.statusCode + " .");
                 var weatherFeed = JSON.parse(body);
                 if (weatherFeed) {
                     test.ok(weatherFeed.statusCode, "StatusCode property should be defined .");
