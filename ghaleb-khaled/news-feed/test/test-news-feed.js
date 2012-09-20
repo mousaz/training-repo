4299dc49baa5bdd92955abbac9c348fdc82faebe
ghalebkhaled <gkhalid@asaltech.com>
2012-09-20 17:02:24 +0300

    adding other test

--- ghaleb-khaled/news-feed/test/test-news-feed.js
+++ ghaleb-khaled/news-feed/test/test-news-feed.js
@@ -2,22 +2,42 @@ var http = require('http');
 var server = 'http://127.0.0.1:8080/news';
 
 
-exports.testItem = function (test) {
-    var resData = "";
-    var trueValue = "Soldier has baby in Afghanistan";
-    http.get(server + '?filter=Afghanistan', function (res) {
+function testPath(path, test, cb) {
+    var resData = new String();
+    http.get(server + path, function (res) {
 
         res.on('data', function (chunk) {
             resData += chunk;
         });
         res.on('end', function () {
-            var newsFeed = JSON.parse(resData);
-            test.equal(newsFeed.news[0].title, trueValue, "retrieved data is not valid");
-            test.done();
+            cb(resData);
         });
     }).on('error', function (e) {
-        test.ok(false, "request failed")
+        test.ok(false, "request failed");
         test.done();
     });
 
 }
+
+exports.testItem = function (test) {
+    var trueValue = "Soldier has baby in Afghanistan";
+    testPath('?filter=Afghanistan', test, function (testValue) {
+        try {
+            test.equal(JSON.parse(testValue).news[0].title, trueValue, "retrieved data is not valid");
+        } catch (e) {
+            test.ok(false, "error:" + e.message);
+        }
+        test.done();
+    });
+}
+exports.testEmpty = function (test) {
+    testPath('?filter=man', test, function (empValue1) {
+        testPath('', test, function (empValue2) {
+            if(empValue1 != empValue2) {
+                test.ok(false," empty test is failed ")
+            }
+            test.done();
+        });
+    });
+}
+
