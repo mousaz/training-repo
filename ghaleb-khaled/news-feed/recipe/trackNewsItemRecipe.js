df709d082e9e03486936d345165cb76b91b447af
ghalebkhaled <gkhalid@asaltech.com>
2012-09-30 11:58:41 +0200

    adding recipe to track specific news using filtering

---
+++ ghaleb-khaled/news-feed/recipe/trackNewsItemRecipe.js
@@ -0,0 +1,66 @@
+var period = "3"; // in minutes
+var filter = "Man";
+
+var firstTime = new Date();
+firstTime.setMinutes(firstTime.getMinutes() + parseInt(period));
+
+
+//display the news on client mobile
+function showNews(news) {
+    news.forEach(function (newsItem) {
+        var notification = device.notifications.createNotification(newsItem.title);
+        notification.content = newsItem.description;
+        notification.on('click',
+            function () {
+                var messageBox = device.notifications.createMessageBox(newsItem.title);
+                messageBox.content =newsItem.description;
+                messageBox.buttons = [ 'Show', 'Hide' ];
+
+                messageBox.on('Show', function () {
+                    device.browser.launch(newsItem.link);
+                });
+                messageBox.show();
+            });
+        notification.show();
+    });
+}
+
+
+//getting news-feed from the webservice
+function getNewsData() {
+    var news;
+    device.ajax(
+        {
+            url: 'http://news-feed.mousaz.msproto.net/news?filter=' + filter,
+            type: 'GET',
+            headers: {
+                'Content-Type': 'application/xml'
+            }
+        },
+        function onSuccess(body, textStatus, response) {
+            if (!(body && (news = JSON.parse(body).news))) {
+                var error = {};
+                error.message = 'invalid body format';
+                error.content = body;
+                console.error('error: ',error);
+            } else {
+                showNews(news);
+            }
+        },
+        function onError(textStatus, response) {
+            var error = {};
+            error.message = textStatus;
+            error.statusCode = response.status;
+            console.error('error: ', error);
+        }
+    );
+}
+
+//setting schedulers
+device.scheduler.setTimer({
+    name: 'NewsTracker',
+    time: firstTime.getTime(),
+    interval: period * 1000,
+    repeat: true,
+    exact: true
+}, getNewsData);
