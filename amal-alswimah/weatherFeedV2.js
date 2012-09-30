f15280d345d83fd5fcb8e410948d2190500af2ef
Alswimah <aswimah@asaltech.com>
2012-09-30 11:11:31 +0200

    new modifications on weatherFeedV2.js

--- amal-alswimah/weatherFeedV2.js
+++ amal-alswimah/weatherFeedV2.js
@@ -4,9 +4,9 @@ var express = require("express"),
     xml2js = require('xml2js'),
     parser = new xml2js.Parser();
 /**
- * parsing the body of requested page and print it in response body as JSON object
- * @param res , response
- * @param body , body
+ * parses the body of requested page and print it in response body as JSON object
+ * @param res; response
+ * @param body; body
  */
 function parseToJson(res, body) {
     parser.parseString(body, function (err, result) {
@@ -15,17 +15,17 @@ function parseToJson(res, body) {
                 console.error("Error happened during parsing the body .");
                 var errorObject =
                 {
-                    statusCode: 500,
-                    message: "Error : error occurred during retrieving" +
+                    statusCode:500,
+                    message:"Error : error occurred during retrieving" +
                         "the data .Error =  " + err + " ."
                 }
-                res.writeHead(500, {"Content-Type": "application/json"});
+                res.writeHead(500, {"Content-Type":"application/json"});
                 return res.end(JSON.parse(errorObject));
             }
             var errorObject =
             {
-                statusCode: 400,
-                message: " sorry , this application does not support your city name ."
+                statusCode:400,
+                message:" sorry , this application does not support your city name ."
             };
             var property = "result";
             if (!result) {
@@ -42,7 +42,7 @@ function parseToJson(res, body) {
                 return printError(res, errorObject, property);
             }
             var weatherItem = weather[0];
-            property = " [0] ";
+            property = " weather[0] ";
             if (!weatherItem) {
                 return printError(res, errorObject, property);
             }
@@ -55,8 +55,8 @@ function parseToJson(res, body) {
             var skytext = result.weatherdata.weather[0].current[0]["$"].skytext;
             var weatherObject =
             {
-                temperature: temperature,
-                skytext: skytext
+                temperature:temperature,
+                skytext:skytext
             };
             console.log("retrieving the data is successful .");
             res.end(JSON.stringify(weatherObject));
@@ -64,14 +64,14 @@ function parseToJson(res, body) {
     );
 }
 /**
- * print errorObject which is a json object
- * @param res , response
- * @param errorObject , Json object
- * @param property , string
+ * prints errorObject which is a json object
+ * @param res; response
+ * @param errorObject; Json object
+ * @param property; string
  */
 function printError(res, errorObject, property) {
     console.log(" " + property + " property is undefined .");
-    res.writeHead(400, {"Content-Type": "application/json"});
+    res.writeHead(400, {"Content-Type":"application/json"});
     res.end(JSON.stringify(errorObject));
 }
 weatherApp.get("/weather", function (req, res) {
@@ -79,11 +79,11 @@ weatherApp.get("/weather", function (req, res) {
             var location = req.query.location;
             if (!location) {
                 console.log("inside location test .");
-                res.writeHead(400, {"Content-Type": "application/json"});
+                res.writeHead(400, {"Content-Type":"application/json"});
                 var errorObject =
                 {
-                    statusCode: 400,
-                    message: "Error : invalid argument , you must enter  city name " +
+                    statusCode:400,
+                    message:"Error : invalid argument , you must enter  city name " +
                         "(  city name , ex : location = ramallah )that you " +
                         "want to know about it's weather ."
                 }
@@ -92,29 +92,29 @@ weatherApp.get("/weather", function (req, res) {
             console.log("make http request .");
             var xmlData = "";
             var options = {
-                host: 'weather.partners.msn.com',
-                port: 80,
-                path: '/find.aspx?weasearchstr=' + location,
-                method: 'get'
+                host:'weather.partners.msn.com',
+                port:80,
+                path:'/find.aspx?weasearchstr=' + location,
+                method:'get'
             };
-            var internalRequest = http.request(options, function(internalResponse) {
-            console.log("inside http request module .");
-            console.log('STATUS: ' + internalResponse.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(internalResponse.headers));
-            internalResponse.setEncoding('utf8');
-            internalResponse.on('data', function (chunck) {
-                console.log("inside read data from server .");
-                console.log('BODY: ' + chunck);
-                xmlData += chunck;
-            });
-                internalResponse.on("end",function() {
-                console.error("read data");
-                parseToJson(res, xmlData);
+            var internalRequest = http.request(options, function (internalResponse) {
+                console.log("inside http request module .");
+                console.log('STATUS: ' + internalResponse.statusCode);
+                console.log('HEADERS: ' + JSON.stringify(internalResponse.headers));
+                internalResponse.setEncoding('utf8');
+                internalResponse.on('data', function (chunck) {
+                    console.log("inside read data from server .");
+                    console.log('BODY: ' + chunck);
+                    xmlData += chunck;
+                });
+                internalResponse.on("end", function () {
+                    console.log("read data");
+                    parseToJson(res, xmlData);
                 });
             });
-            internalRequest.on('error', function(e) {
-                console.log('problem with request: ' + e);
-                res.end("error : "+e);
+            internalRequest.on('error', function (e) {
+                console.error('problem with request: ' + e);
+                res.end("error : " + e);
             });
             internalRequest.end();
         } catch (exception) {
